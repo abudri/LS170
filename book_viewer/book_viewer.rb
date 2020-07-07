abc8db6ab844a64cbd902ecf7fff9baeff8c4869
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-07 15:33:28 -0400

    updating the formatting of the table of contents from an assignment

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -4,6 +4,6 @@ require "sinatra/reloader"
 
 get "/" do
   @title = "Abdullah's Book Viewer App"
-  @table_of_contents = File.read("data/toc.txt")
+  @table_of_contents = File.readlines("data/toc.txt")
   erb :home  # gets views/home/erb.  recall this is like the erb() method you wrote in the 4 parts series on using Rack app and making a makeshift framwork in earlier assignment
 end
