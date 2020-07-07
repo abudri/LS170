c28403bf704806d9f2d90740f74f186f9dddd4ed
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-07 15:08:25 -0400

    initial Book Viewer project, Lesson 3, with very little initial changes

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,9 @@
+require "tilt/erubis" # lesson 3, Assignment: https://launchschool.com/lessons/c3578b91/assignments/d6969b5b
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @title = "Abdullah's Book Viewer App"
+  @table_of_contents = File.read("data/toc.txt")
+  erb :home  # gets views/home/erb.  recall this is like the erb() method you wrote in the 4 parts series on using Rack app and making a makeshift framwork in earlier assignment
+end
