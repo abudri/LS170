822f4a7d5655d4fb3e8ca6188c3bde27e8cea4a8
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2020-07-08 11:27:58 -0400

    added a main layout.erb and updated some views

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -4,6 +4,17 @@ require "sinatra/reloader"
 
 get "/" do
   @title = "Abdullah's Book Viewer App"
-  @table_of_contents = File.readlines("data/toc.txt")
+  @contents = File.readlines("data/toc.txt") # formerly @table_of_contents.  This is Table of Contents
   erb :home  # gets views/home/erb.  recall this is like the erb() method you wrote in the 4 parts series on using Rack app and making a makeshift framwork in earlier assignment
+end
+
+# get "/chapters/:number" do
+#   @chapter_contents = File.readlines("data/{:number}.text")
+# end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt") # formerly @table_of_contents.  This is Table of Contents
+  @chapter = File.read("data/chp1.txt") # formerly @chapter_contents.  This is a chapter's contents
+  erb :chapter
 end
